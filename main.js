8514f1a2e9ceb844d5374f7b4237f824606d7e4d
El Rubio <879899@stud.unive.it>
2024-04-15 22:33:55 +0200

    fix main

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
 import puppeteer from 'puppeteer';
 import fs from 'fs'
 
+class ExecutionManager {
+
+    #ANSI_RESET = '\x1b[0m';
+    #ANSI_COLOR_RED = '\x1b[31m';
+    #ANSI_COLOR_GREEN = '\x1b[32m';
+    #ANSI_COLOR_YELLOW = '\x1b[33m';
+
+    #currentState = {
+        validResults: 0, 
+        undefinedResults: 0, 
+        unreachableResults: 0
+    };
+
+    update(value) {
+        if (value.result === "unreachable") {
+            this.#currentState.unreachableResults += 1;
+        } else if (value.result === "undefined") {
+            this.#currentState.undefinedResults += 1;
+        } else {
+            this.#currentState.validResults += 1;
+        }
+    }
+
+    logState(totalData) {
+
+        const state = this.#currentState;
+        const validResults = `Valid results: ${this.#ANSI_COLOR_GREEN}${state.validResults}${this.#ANSI_RESET}`;
+        const undefinedResults = `Undefined results: ${this.#ANSI_COLOR_RED}${state.undefinedResults}${this.#ANSI_RESET}`;
+        const unreachableResults = `Unreachable results: ${this.#ANSI_COLOR_YELLOW}${state.unreachableResults}${this.#ANSI_RESET}`;
+        const sum = state.undefinedResults + state.unreachableResults + state.validResults;
+
+        process.stdout.write("\r\x1b[K");
+        process.stdout.write(`${validResults}; ${undefinedResults}; ${unreachableResults} (${sum / totalData * 100}%)`);
+    }
+}
+
+
+const CHUNK_SIZE = 20;
 const path_in = process.argv[2];
 const path_out = process.argv[3];
 const browser = await puppeteer.launch();
@@ -18,16 +56,37 @@ const text = await streamAsPromise(fs.createReadStream(path_in));
 const domains = text.split('\n').map((text) => (text.split(',')[1]).replace("\r", ""));
 
 async function fetchResults(domains) {
-    return await Promise.all(domains.map(async (domain) => {
-        try {
+
+    const manager = new ExecutionManager();
+    const chunks = splitInChunks(domains, CHUNK_SIZE);
+    const resolved = []
+    const finalStep = (value) => {
+        manager.update(value);
+        manager.logState(domains.length);
+        return value;
+    }
+
+    for (const chunk of chunks) {
+        const currentResults = await Promise.all(chunk.map(async (domain) => {
+
             const page = await browser.newPage();
-            const response = await page.goto("https://" + domain);
-            const headers = response.headers();
-            return createDict(domain, headers['strict-transport-security'] ?? "undefined");
-        } catch (error) {
-            return createDict(domain, "unreachable")
-        }
-    }));
+            let value = null;
+            try {
+                const response = await page.goto("https://" + domain);
+                const headers = response.headers();
+                value = createDict(domain, headers['strict-transport-security'] ?? "undefined");
+            } catch (error) {
+                value = createDict(domain, "unreachable")
+            } finally {
+                await page.close();
+            }
+
+            return finalStep(value);
+        }));
+        
+        resolved.push(currentResults);
+    }
+    return resolved.flat();
 }
 
 const results = await fetchResults(domains);
@@ -72,4 +131,17 @@ function createDict(domain, result) {
     };
   
     return data;
+}
+
+function splitInChunks(array, size) {
+
+    let currentIndex = 0;
+    const chunks = [];
+
+    while (currentIndex <= array.length - 1) {
+        chunks.push(array.slice(currentIndex, currentIndex + size));
+        currentIndex += size;
+    }
+
+    return chunks;
 }
